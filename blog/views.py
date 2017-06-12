d21116e29348ac4e065763198e2ace53aaedef0b
Your Name <you@example.com>
2017-06-12 18:51:56 +0800

    3th commit

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,21 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render,HttpResponse, get_object_or_404
-from django.template import Context,Template
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import *
 from . import models
 
 def index(request):
-    aritcle = get_object_or_404(Aritcle)
-    # aritcle = models.Aritcle.browse_num
-    num = aritcle.browse_num
-    num = num+1
-    aritcle.browse_num=num
-    aritcle.save()
-
     context = {}
+    # 取得全部文章放放到aritcle_list
     aritcle_list = Aritcle.objects.all()
+    paginator = Paginator(aritcle_list, 3)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
     context['aritcle_list'] = aritcle_list
     index_page = render(request, 'index.html', context)
     # render第三个参数是后台传送到前端的数据
@@ -25,6 +23,12 @@ def index(request):
     return index_page
 
 def aritcle_page(request,aritcle_id):
+    aritcle = get_object_or_404(Aritcle,pk=aritcle_id)
+    # aritcle = models.Aritcle.browse_num
+    num = aritcle.browse_num
+    num = num + 1
+    aritcle.browse_num = num
+    aritcle.save()
     context={}
     # aritcle=Aritcle.objects.all()
     aritcle=models.Aritcle.objects.get(pk=aritcle_id)
@@ -36,4 +40,8 @@ def aritcle_page(request,aritcle_id):
 #     num += 1
 #     post.browse_num = num
 #     post.save()
+#  class index(Listview):
+#
+#      model = Aritcle
+
 
--- laohu/settings.py
+++ laohu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'htewc^inr%@2&0u305kazqfm*6*epa3tvg+=$dp4_5sh0jq$et'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','182.254.230.138']
 
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR,'static')
+# STATIC_ROOT =  os.path.join(BASE_DIR,'static')
